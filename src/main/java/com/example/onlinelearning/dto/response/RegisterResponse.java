f2d02d1629247794a6b628f9a25e966a00b38a50
seif20045 <seifehab8x@gmail.com>
2025-12-28 00:12:02 +0200

    "feat: add RegisterRequest ,Response DTO "

---
+++ src/main/java/com/example/onlinelearning/dto/response/RegisterResponse.java
@@ -0,0 +1,10 @@
+package com.example.onlinelearning.dto.response;
+
+import lombok.Data;
+
+@Data
+public class RegisterResponse {
+    private Integer id;
+    private String name;
+    private String email;
+}
