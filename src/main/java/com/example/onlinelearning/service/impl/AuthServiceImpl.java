2355ad78c80eeecd245daeb5df6b380ae01c332c
seif20045 <seifehab8x@gmail.com>
2025-12-27 18:17:36 +0200

    "feat: implement login service "

---
+++ src/main/java/com/example/onlinelearning/service/impl/AuthServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.onlinelearning.service.impl;
+
+import com.example.onlinelearning.dto.request.LoginRequest;
+import com.example.onlinelearning.dto.response.LoginResponse;
+import com.example.onlinelearning.entity.User;
+import com.example.onlinelearning.exception.BusinessException;
+import com.example.onlinelearning.repository.UserRepository;
+import com.example.onlinelearning.service.IAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements IAuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * @param loginRequest
+     * @return
+     */
+    @Override
+    public LoginResponse login(LoginRequest loginRequest) {
+
+        User user = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(
+                () -> new BusinessException("Invalid email or password"));
+
+        if (passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+
+            throw new BusinessException("Invalid email or password");
+        }
+
+        LoginResponse response = new LoginResponse();
+        response.setId(user.getId());
+        response.setName(user.getName());
+        response.setEmail(user.getEmail());
+        return response;
+    }
+}
